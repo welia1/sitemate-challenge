8f5b1f1d6e5a0ed46e18f3fe1ff02e6712f508c0
Wenfei Lian <wenfei@foundr.com>
2023-05-17 18:07:38 +1000

    keep client running until quit

--- client/client.js
+++ client/client.js
@@ -5,53 +5,61 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:3000/issues';
 
 async function main() {
-  const { action } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'action',
-      message: 'What do you want to do?',
-      choices: ['Create', 'Read', 'Update', 'Delete', 'List'],
-    },
-  ]);
+  while (true) {
+    const { action } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What do you want to do?',
+        choices: ['Create', 'Read', 'Update', 'Delete', 'List', 'Quit'],
+      },
+    ]);
 
-  let id;
-  switch (action) {
-    case 'Create':
-      const { title, description } = await inquirer.prompt([
-        { type: 'input', name: 'title', message: 'Enter the title:' },
-        { type: 'input', name: 'description', message: 'Enter the description:' },
-      ]);
-      await axios.post(baseUrl, { title, description });
-      console.log('Issue created successfully');
+    if (action === 'Quit') {
+      console.log('Goodbye!');
       break;
+    }
 
-    case 'Read':
-      id = await getIssueId();
-      const issue = await axios.get(`${baseUrl}/${id}`);
-      console.log(issue.data);
-      break;
+    let id;
+    switch (action) {
+      case 'Create':
+        const { title, description } = await inquirer.prompt([
+          { type: 'input', name: 'title', message: 'Enter the title:' },
+          { type: 'input', name: 'description', message: 'Enter the description:' },
+        ]);
+        await axios.post(baseUrl, { title, description });
+        console.log('Issue created successfully');
+        break;
 
-    case 'Update':
-      id = await getIssueId();
-      const update = await inquirer.prompt([
-        { type: 'input', name: 'title', message: 'Enter the new title:' },
-        { type: 'input', name: 'description', message: 'Enter the new description:' },
-      ]);
-      await axios.put(`${baseUrl}/${id}`, update);
-      console.log('Issue updated successfully');
-      break;
+      case 'Read':
+        id = await getIssueId();
+        const issue = await axios.get(`${baseUrl}/${id}`);
+        console.log(issue.data);
+        break;
 
-    case 'Delete':
-      id = await getIssueId();
-      await axios.delete(`${baseUrl}/${id}`);
-      console.log('Issue deleted successfully');
-      break;
+      case 'Update':
+        id = await getIssueId();
+        const update = await inquirer.prompt([
+          { type: 'input', name: 'title', message: 'Enter the new title:' },
+          { type: 'input', name: 'description', message: 'Enter the new description:' },
+        ]);
+        await axios.put(`${baseUrl}/${id}`, update);
+        console.log('Issue updated successfully');
+        break;
 
-    case 'List':
-      const issues = await axios.get(baseUrl);
-      console.log(issues.data);
-      break;
+      case 'Delete':
+        id = await getIssueId();
+        await axios.delete(`${baseUrl}/${id}`);
+        console.log('Issue deleted successfully');
+        break;
+
+      case 'List':
+        const issues = await axios.get(baseUrl);
+        console.log(issues.data);
+        break;
+    }
   }
+  process.exit(0);
 }
 
 async function getIssueId() {
